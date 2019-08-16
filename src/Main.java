f18b5baaf0937ee9533be84e87f4a2b668fd0abe
Flibielt <flibielt@Inspiron>
2019-08-16 14:04:49 +0200

    Added functional console interface.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String text = "árvíztűrő tükörfúrógép";
-		String codedText = WeightCode.getCode(text);
-		System.out.println(codedText);
-		try {
-			System.out.println(WeightCode.decode(codedText));
-		} catch (Exception e) {
-			System.out.println(e.toString());
+		System.out.println("0: Decode");
+		System.out.println("1: Code");
+		
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		System.out.print("Response: ");
+		String response = in.nextLine();
+		while (response.charAt(0) != '0' && response.charAt(0) != '1') {
+			System.out.println("Unexpected response!");
+			System.out.print("Response: ");
+			response = in.nextLine();
+		}
+		
+		System.out.print("The text: ");
+		String text = in.nextLine();
+		if (response.charAt(0) == '0') {
+			try {
+				System.out.println(WeightCode.decode(text));
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		} else {
+			System.out.println(WeightCode.getCode(text));
 		}
 	}
 
