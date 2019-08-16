4a467eb7cfad0de40cc1a906fe27d3ad05a9ca85
Flibielt <flibielt@Inspiron>
2019-08-16 13:47:28 +0200

    Added serial version UID to the WeightCodeException class.

--- src/WeightCodeException.java
+++ src/WeightCodeException.java
@@ -1,5 +1,10 @@
 
 public class WeightCodeException extends Exception {
+	/**
+	 * The serial version UID of the class
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public WeightCodeException(String message) {
 		super(message);
 	}
